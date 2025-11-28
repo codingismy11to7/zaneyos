c25fbb6e130534d82610bcf30dd3e5e285014638
dwilliams <don.e.williams@gmail.com>
2025-11-27 21:18:42 -0500

    Updated flake moving to 25.11
     On branch migrate-2511
     Your branch is up to date with 'origin/migrate-2511'.
    
     Changes to be committed:
    	modified:   flake.lock
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,12 @@
 
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     nvf.url = "github:notashelf/nvf";
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-25.11";
     nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
 
     # QuickShell (optional add quickshell to outputs to enable)
