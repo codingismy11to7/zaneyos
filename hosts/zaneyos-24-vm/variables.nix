08095c7ad02947b9b39b7d0dd1dfebf8e0879032
dwilliams <don.e.williams@gmail.com>
2025-11-28 15:42:38 -0500

    Added fix script for emacs doom iedit issue
     On branch migrate-2511
     Your branch is up to date with 'origin/migrate-2511'.
    
     Changes to be committed:
    	modified:   hosts/zaneyos-24-vm/variables.nix
    	new file:   modules/fix-emacs-iedit.sh

--- hosts/zaneyos-24-vm/variables.nix
+++ hosts/zaneyos-24-vm/variables.nix
@@ -78,8 +78,8 @@
   # Add new images to ~/zaneyos/wallpapers
   #stylixImage = ../../wallpapers/mountainscapedark.jpg;
   #stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
-  stylixImage = ../../wallpapers/Anime-Purple-eyes.png;
-  #stylixImage = ../../wallpapers/Rainnight.jpg;
+  #stylixImage = ../../wallpapers/Anime-Purple-eyes.png;
+  stylixImage = ../../wallpapers/Rainnight.jpg;
   #stylixImage = ../../wallpapers/zaney-wallpaper.jpg;
   #stylixImage = ../../wallpapers/nix-wallpapers-strips-logo.jpg;
   #stylixImage = ../../wallpapers/beautifulmountainscape.jpg;
