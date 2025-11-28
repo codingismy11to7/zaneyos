b5ca806c4e2d3c17a9b07cdc039c710c114a9895
Don Williams <don.e.williams@gmail.com>
2025-11-28 16:03:19 -0500

    Added animations and waybars from ddubosOS
     On branch migrate-2511
     Your branch is up to date with 'origin/migrate-2511'.
    
     Changes to be committed:
    	modified:   hosts/default/variables.nix
    	modified:   hosts/nixstation/variables.nix
    	modified:   hosts/zaneyos-24-vm/variables.nix
    	modified:   hosts/zaneyos-oem/variables.nix
    	new file:   modules/home/hyprland/animations-end-slide.nix
    	new file:   modules/home/hyprland/animations-end4-slide.nix
    	new file:   modules/home/hyprland/animations-hyde-optimized.nix
    	new file:   modules/home/hyprland/animations-mahaveer-me-1.nix
    	new file:   modules/home/hyprland/animations-mahaveer-me-2.nix
    	new file:   modules/home/hyprland/animations-ml4w-classic.nix
    	new file:   modules/home/hyprland/animations-ml4w-fast.nix
    	new file:   modules/home/hyprland/animations-ml4w-high.nix
    	new file:   modules/home/waybar/Jerry-waybar.nix
    	new file:   modules/home/waybar/waybar-TheBlackDon.nix
    	new file:   modules/home/waybar/waybar-ddubsos-v1.nix
    	new file:   modules/home/waybar/waybar-dwm2.nix
    	new file:   modules/home/waybar/waybar-jak-catppuccin.nix
    	new file:   modules/home/waybar/waybar-jak-ml4w-modern.nix
    	new file:   modules/home/waybar/waybar-jak-oglo-simple.nix
    	new file:   modules/home/waybar/waybar-jwt-catppuccin.nix
    	new file:   modules/home/waybar/waybar-jwt-transparent.nix
    	new file:   modules/home/waybar/waybar-jwt-ultradark.nix
    	new file:   modules/home/waybar/waybar-mangowc-jak-catppuccin.nix
    	new file:   modules/home/waybar/waybar-mecha.nix
    	new file:   modules/home/waybar/waybar-old-ddubsos.nix
    	new file:   modules/home/waybar/waybar-pctrade-catppuccin.nix
    	new file:   modules/home/waybar/waybar-tony.nix

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -90,24 +90,38 @@
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
-  # Throwback to ZaneyOS v1.0
-  #waybarChoice = ../../modules/home/waybar/waybar-jerry.nix;
-  # Very colorful and bright colors
-  #waybarChoice = ../../modules/home/waybar/waybar-nekodyke.nix;
-  # DWM styled waybars from Matt at TheLinuxCast
   #waybarChoice = ../../modules/home/waybar/waybar-dwm.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-dwm-2.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-nekodyke.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jerry.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-TheBlackDon.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-tony.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-ddubsos-v1.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-mecha.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jak-catppuccin.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jak-ml4w-modern.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jak-oglo-simple.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jwt-catppuccin.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jwt-transparent.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-jwt-ultradark.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-pctrade-catppuccin.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-mangowc-jak-catppuccin.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-old-ddubsos.nix;
 
   # Set Animation style
   # Available options are:
-  # animations-def.nix  (default)
-  # animations-end4.nix (end-4 project very fluid)
-  # animations-dynamic.nix (ml4w project)
-  # animations-moving.nix (ml4w project)
   animChoice = ../../modules/home/hyprland/animations-def.nix;
   #animChoice = ../../modules/home/hyprland/animations-end4.nix;
+  #animChoice = ../../modules/home/hyprland/animations-end4-slide.nix;
+  #animChoice = ../../modules/home/hyprland/animations-end-slide.nix;
   #animChoice = ../../modules/home/hyprland/animations-dynamic.nix;
   #animChoice = ../../modules/home/hyprland/animations-moving.nix;
+  #animChoice = ../../modules/home/hyprland/animations-hyde-optimized.nix;
+  #animChoice = ../../modules/home/hyprland/animations-mahaveer-me-1.nix;
+  #animChoice = ../../modules/home/hyprland/animations-mahaveer-me-2.nix;
+  #animChoice = ../../modules/home/hyprland/animations-ml4w-classic.nix;
+  #animChoice = ../../modules/home/hyprland/animations-ml4w-fast.nix;
+  #animChoice = ../../modules/home/hyprland/animations-ml4w-high.nix;
 
   # Set network hostId if required (needed for zfs)
   # Otherwise leave as-is
