91d406aed34fb8ef0bde5b56f4fc2e89506cb5cd
Steven Scott <codingismy11to7@gmail.com>
2025-12-04 01:06:36 -0500

    add secrets

---
+++ modules/core/sops.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  username,
+  ...
+}: {
+  sops = {
+    defaultSopsFile = ../../secrets/secrets.yaml;
+
+    age.keyFile = config.zaneyos.sopsKeyFile;
+
+    secrets = {
+      unixPassword = {
+        neededForUsers = true;
+      };
+      unixRootPassword = {
+        neededForUsers = true;
+      };
+    };
+  };
+
+  # When running in a VM (via nh os build-vm), mount the sops directory
+  # so sops can access the key.
+  virtualisation.vmVariant = {
+    virtualisation.sharedDirectories = {
+      secrets = {
+        source = "/home/${username}/zaneyos/secrets";
+        target = "/home/${username}/zaneyos/secrets";
+      };
+
+      sops-keys = {
+        source = dirOf config.zaneyos.sopsKeyFile;
+        target = dirOf config.zaneyos.sopsKeyFile;
+      };
+    };
+  };
+}
