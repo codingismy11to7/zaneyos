061d89a76bdf4b1f8138457eedc9fe815432f23f
Steven Scott <codingismy11to7@gmail.com>
2025-12-06 12:25:17 -0500

    add unstable packages

--- modules/core/overlays.nix
+++ modules/core/overlays.nix
@@ -3,5 +3,11 @@
   nixpkgs.overlays = [
     # Provide pkgs.google-antigravity via antigravity-nix overlay
     inputs.antigravity-nix.overlays.default
+
+    (_final: prev: {
+      unstable = import inputs.nixpkgs-unstable {
+        inherit (prev) system;
+      };
+    })
   ];
 }
