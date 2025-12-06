96329fd9b021ea5ce3fcda092165cfd1faf64894
Steven Scott <codingismy11to7@gmail.com>
2025-12-06 12:25:27 -0500

    add lazyvim config

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -21,10 +21,10 @@
   ];
 in {
   programs = {
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
+    # neovim = {
+    #   enable = true;
+    #   defaultEditor = true;
+    # };
     firefox.enable = false; # Firefox is not installed by default
     hyprland = {
       enable = true; # set this so desktop file is created
