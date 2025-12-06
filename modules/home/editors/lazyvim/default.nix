1f9bd3376fe4a5ca595cd41a4f8e553674225e67
Steven Scott <codingismy11to7@gmail.com>
2025-12-06 12:25:27 -0500

    add lazyvim config

---
+++ modules/home/editors/lazyvim/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/zaneyos/modules/home/editors/lazyvim/config";
+
+  home.packages = let
+    stablePkgs = with pkgs; [
+      cargo
+      gcc
+      nodejs
+    ];
+    unstablePkgs = with pkgs.unstable; [
+      fd
+      neovim
+      nil
+      nixfmt-rfc-style
+      ripgrep
+      statix
+      tree-sitter
+    ];
+  in
+    stablePkgs ++ unstablePkgs;
+}
