af29bfd5afe5981b27580baec089dd02f910596b
Steven Scott <codingismy11to7@gmail.com>
2025-12-13 14:33:29 -0500

    remove some packages

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -38,7 +38,7 @@ in {
       ./cli/gh.nix
       ./cli/git.nix
       ./gtk.nix
-      ./cli/htop.nix
+      # ./cli/htop.nix
       ./hyprland
       ./terminals/kitty.nix
       ./cli/lazygit.nix
@@ -50,7 +50,7 @@ in {
       ./rofi
       ./qt.nix
       ./scripts
-      ./scripts/gemini-cli.nix
+      # ./scripts/gemini-cli.nix
       ./stylix.nix
       ./swappy.nix
       ./swaync.nix
@@ -60,7 +60,7 @@ in {
       ./wlogout
       ./xdg.nix
       ./yazi
-      ./zen-browser.nix
+      # ./zen-browser.nix
       ./zoxide.nix
       ./zsh
     ]
