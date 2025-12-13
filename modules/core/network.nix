369dc0f950638a1ba5f546713c4967adf0be84ec
Steven Scott <codingismy11to7@gmail.com>
2025-12-13 15:52:30 -0500

    disable firewall

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -10,11 +10,11 @@ in
 {
   networking = {
     hostName = "${host}";
-    hostId = hostId;
+    inherit hostId;
     networkmanager.enable = true;
     timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
     firewall = {
-      enable = true;
+      enable = false;
       allowedTCPPorts = [
         22
         80
